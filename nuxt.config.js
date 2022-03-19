abd9dcbab2233e37882ae7d8aad3c3d37911f948
mauricio <mauricio.vera.r7@gmail.com>
2022-03-19 13:42:42 -0300

    img

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
   router: {
-    base: '/frases/'
+    base: '/phrases/'
   },
   head: {
     titleTemplate: '%s - frases',
