e1008c247d2d7aa6af3101d8fc2e3e4ca49f75a2
mauricio <mauricio.vera.r7@gmail.com>
2022-03-19 12:29:04 -0300

    frases

--- d.sh
+++ d.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+#**agregar estas  lineas en nuxt.config debajo de // Global
+#export default {
+#  // Global page headers: https://go.nuxtjs.dev/config-head
+#  target: 'static',
+#  router: {
+#    base: '/phrases/'
+#  },
+#Instalamos la librería push-dir que nos permitirá realizar el deploy
+#npm install push-dir --save-dev
+#**agregar deploy a json
+# "start": "nuxt start",
+#  "deploy": "push-dir --dir=dist --branch=gh-pages --cleanup",
+#}
+#**subir cambios a github
+#npm run generate   (nuxt generate) (esto genera dist directorio)
+#npm run deploy
+#habilitar gh-pages en github
+#repositorio debe ser publico, no privado
+
 git add .
 git commit -m 'frases'
 git push
